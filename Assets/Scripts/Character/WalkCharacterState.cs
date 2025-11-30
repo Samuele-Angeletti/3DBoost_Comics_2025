0551a38df608b85bf7cbb28c739a3021efe99d4e
Samuele-Angeletti <104771865+Samuele-Angeletti@users.noreply.github.com>
2025-11-30 10:54:45 +0100

    Lezione 9

--- Assets/Scripts/Character/WalkCharacterState.cs
+++ Assets/Scripts/Character/WalkCharacterState.cs
@@ -25,34 +25,33 @@ namespace Assets.Scripts.Character
 
         public override void OnUpdate()
         {
-            _owner.transform.rotation = 
-                Quaternion.Euler(
-                    new Vector3(
-                        _owner.transform.rotation.eulerAngles.x,
-                        Mathf.Lerp(_owner.transform.rotation.eulerAngles.y, _owner.CameraTransform.rotation.eulerAngles.y, _owner.RotationSpeed * Time.fixedTime),
-                        _owner.transform.rotation.eulerAngles.z
-                        )
-                    );
-
             if (_owner.Direction == Vector3.zero)
             {
                 _owner.SetIdle();
                 return;
             }
+
+            // get the target rotation based on movement direction
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            // gradually rotate to target rotation
+            _owner.transform.rotation = Quaternion.RotateTowards(
+                _owner.transform.rotation,
+                targetRotation,
+                _owner.RotationSpeed * Time.deltaTime);
         }
 
-        //Vector3 dir = 
-        //    _owner.transform.forward * _owner.Direction.z + 
-        //    _owner.transform.right * _owner.Direction.x + 
-        //    _owner.transform.up * _owner.Direction.y;
+        private Vector3 moveDirection;
+
         public override void OnFixedUpdate()
         {
-            Vector3 dir2 = _owner.CameraTransform.TransformDirection(_owner.Direction);
+            // get the movement direction relative to the camera
+            moveDirection = _owner.CameraTransform.TransformDirection(_owner.Direction).normalized;
+            // null the camera tilt
+            moveDirection.y = 0;
 
-            _rigidbody.AddForce(
-                _owner.Acceleration * 
-                Time.fixedDeltaTime *
-                dir2);
+            Vector3 velocity = _owner.MaxSpeed * Time.fixedDeltaTime * moveDirection;
+            velocity.y = _rigidbody.linearVelocity.y;
+            _rigidbody.linearVelocity = velocity;
 
             _rigidbody.linearVelocity =
                 new Vector3(
